6eb16ee17c929ffbd523a12c8574dd03e3b6316d
ANVAR SADIQ <anvarsadiqp@users.noreply.github.com>
2016-09-24 12:02:04 +0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+error_reporting(0);
+session_start();
+  if(isset($_SESSION['start'])){
+	  if (file_exists('hit.txt')) {
+          $fil = fopen('hit.txt', r);
+          $dat = fread($fil, filesize('hit.txt'));
+          echo "Hit:".$dat;
+          }
+    }
+  else{
+	  if (file_exists('hit.txt')){
+	  $fil = fopen('hit.txt', r);
+	  $dat = fread($fil, filesize('hit.txt')); 
+	  echo $dat+1;
+	  fclose($fil);
+	  $fil = fopen('hit.txt', w);
+	  fwrite($fil, $dat+1);
+	  }
+	$_SESSION['start'] = "started";
+}
+?>
